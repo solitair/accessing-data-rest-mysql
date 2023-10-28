c80eab89d378907dfd3ef6d253d710350a6c6a9b
solitair <solitair@hitel.net>
2023-10-28 15:37:00 +0900

    expose id field in rest api response.

---
+++ src/main/java/com/example/accessingdatarestmysql/MyRepositoryRestConfigurer.java
@@ -0,0 +1,16 @@
+package com.example.accessingdatarestmysql;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+
+@Configuration
+public class MyRepositoryRestConfigurer implements RepositoryRestConfigurer {
+
+    @Override
+    public void configureRepositoryRestConfiguration(
+      RepositoryRestConfiguration config, CorsRegistry cors) {
+        config.exposeIdsFor(Agent.class);
+    }
+}
